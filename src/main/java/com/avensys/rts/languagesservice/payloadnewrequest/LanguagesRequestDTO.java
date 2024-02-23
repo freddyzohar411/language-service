53e1d45c2af762a451c613dfe67a00ebf1c7cd09
rku-avensys <rahul@aven-sys.com>
2024-02-23 17:03:48 +0530

    removed @data annotation

--- src/main/java/com/avensys/rts/languagesservice/payloadnewrequest/LanguagesRequestDTO.java
+++ src/main/java/com/avensys/rts/languagesservice/payloadnewrequest/LanguagesRequestDTO.java
@@ -1,10 +1,12 @@
 package com.avensys.rts.languagesservice.payloadnewrequest;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class LanguagesRequestDTO {
